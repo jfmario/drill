f04542f7ef916b50e062911f131dfd398d0f9658
Charles Givre <cgivre@apache.org>
2020-03-31 10:03:06 -0400

    [WIP]

--- contrib/format-httpd/src/main/java/org/apache/drill/exec/store/httpd/HttpdParser.java
+++ contrib/format-httpd/src/main/java/org/apache/drill/exec/store/httpd/HttpdParser.java
@@ -17,7 +17,6 @@
  */
 package org.apache.drill.exec.store.httpd;
 
-import org.apache.calcite.interpreter.Row;
 import org.apache.drill.exec.physical.resultSet.RowSetLoader;
 import org.apache.drill.shaded.guava.com.google.common.base.Preconditions;
 import org.apache.drill.shaded.guava.com.google.common.collect.Maps;
