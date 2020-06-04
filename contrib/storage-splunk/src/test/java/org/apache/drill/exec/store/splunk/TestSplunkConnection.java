625f4f6ca8f53e4f623b634596bb4ffc66dfd9e8
Charles Givre <cgivre@apache.org>
2020-06-04 10:08:31 -0400

    Unit Test Updates

--- contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/TestSplunkConnection.java
+++ contrib/storage-splunk/src/test/java/org/apache/drill/exec/store/splunk/TestSplunkConnection.java
@@ -3,13 +3,16 @@ package org.apache.drill.exec.store.splunk;
 import com.splunk.EntityCollection;
 import com.splunk.Index;
 import org.apache.drill.common.exceptions.UserException;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class TestSplunkConnection {
 
   @Test
+  @Ignore("Requires an active Splunk connection.")
   public void testConnection() throws Exception {
     SplunkConnection sc = new SplunkConnection(new SplunkPluginConfig("admin", "password", "localhost", 8089, null, null));
     sc.connect();
@@ -22,11 +25,12 @@ public class TestSplunkConnection {
       sc.connect();
       fail();
     } catch (UserException e) {
-
+      assertTrue(e.getMessage().contains("CONNECTION ERROR: Unable to connect to Splunk"));
     }
   }
 
   @Test
+  @Ignore("Requires an active Splunk connection.")
   public void testGetIndexes() throws Exception {
     SplunkConnection sc = new SplunkConnection(new SplunkPluginConfig("admin", "password", "localhost", 8089, null, null));
     EntityCollection<Index> indexes = sc.getIndexes();
